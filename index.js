ea83ad31dd80732c0b65205cc43a717016bd1987
valahdyo <valahdyo@ilcs.co.id>
2024-08-15 22:20:19 +0700

    Fix cron tab

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ express().listen(3000, () => {
   console.log("Server running on port 3000");
   async function main() {
     await db.initialize();
-    cron.schedule("0 7 * * *", async () => {
+    cron.schedule("0 0 * * *", async () => {
       console.log("[INFO] - Start Procces", new Date().toLocaleString());
       const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
       setTimeout(async () => { await absen('masuk');}, delay);
       console.log("[INFO] - Finish Procces", new Date().toLocaleString());
     });
   
-    cron.schedule("0 20 * * *", async () => {
+    cron.schedule("0 13 * * *", async () => {
       console.log("[INFO] - Start Procces", new Date().toLocaleString());
       const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
       setTimeout(async () => { await absen('keluar');}, delay);
