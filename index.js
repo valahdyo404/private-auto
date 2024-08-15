1d1f044363695c0d2444fcc7707d3211caca0317
valahdyo <valahdyo@ilcs.co.id>
2024-08-15 20:53:12 +0700

    add express

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
+const express = require('express');
 const cron = require("node-cron");
 const { absen } = require("./absen");
 const db = require('./config/db');
 
-async function main() {
-  await db.initialize();
-  cron.schedule("0 7 * * *", async () => {
-    console.log("[INFO] - Start Procces", new Date().toLocaleString());
-    const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
-    setTimeout(async () => { await absen('masuk');}, delay);
-    console.log("[INFO] - Finish Procces", new Date().toLocaleString());
-  });
+express().listen(3000, () => {
+  console.log("Server running on port 3000");
+  async function main() {
+    await db.initialize();
+    cron.schedule("0 7 * * *", async () => {
+      console.log("[INFO] - Start Procces", new Date().toLocaleString());
+      const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
+      setTimeout(async () => { await absen('masuk');}, delay);
+      console.log("[INFO] - Finish Procces", new Date().toLocaleString());
+    });
   
-  cron.schedule("0 20 * * *", async () => {
-    console.log("[INFO] - Start Procces", new Date().toLocaleString());
-    // const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
-    setTimeout(async () => { await absen('keluar');}, 100);
-    console.log("[INFO] - Finish Procces", new Date().toLocaleString());
-  });
-}main();
+    cron.schedule("0 20 * * *", async () => {
+      console.log("[INFO] - Start Procces", new Date().toLocaleString());
+      // const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
+      setTimeout(async () => { await absen('keluar');}, 100);
+      console.log("[INFO] - Finish Procces", new Date().toLocaleString());
+    });
+  }main();
+})
