6a05a3e433d7f311c2a37e73bf770832e4019af7
valahdyo <valahdyo@ilcs.co.id>
2024-08-16 08:02:13 +0700

    Fix log process

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const express = require('express');
 const cron = require("node-cron");
 const { absen } = require("./absen");
 const db = require('./config/db');
+const app = express();
 
-express().listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server running on port 3000");
+  app.get("/", async (req, res, next) => {
+		try {
+			const data = {
+				status: "Health Check Success",
+				date: new Date().toLocaleString(),
+				uptime: `${Math.round(process.uptime())} second`
+			};
+			res.status(200).json({
+				msg: "Auto Service",
+				data
+			});
+		} catch (error) {
+			next(error);
+		}
+	});
   async function main() {
     await db.initialize();
-    cron.schedule("0 0 * * *", async () => {
+    cron.schedule("*/5 * * * *", async () => {
       console.log("[INFO] - Start Procces", new Date().toLocaleString());
       const delay = ((Math.round(Math.random() * 30) + 2) * 60 * 1000)
       setTimeout(async () => { await absen('masuk');}, delay);
@@ -21,7 +37,7 @@ express().listen(3000, () => {
       setTimeout(async () => { await absen('keluar');}, delay);
       console.log("[INFO] - Finish Procces", new Date().toLocaleString());
     });
-    cron.schedule("0 * * * *", async () => {
+    cron.schedule("* * * * *", async () => {
       console.log("[INFO] - Log Process", new Date().toLocaleString()); 
     });
   }main();
